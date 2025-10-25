9ab29c85e9eadefa8e47aa2c82bb0021c425d168
Poojii001 <poojapal5781@gmail.com>
2025-10-25 17:52:23 +0530

    'Java-files'

--- String/strQ.java
+++ String/strQ.java
@@ -22,7 +22,7 @@ count++;
 System.out.println(count);
 }
 }
-*/
+
 //wap to accept a code which is contains only a digits.
 import java.util.*;
 class Q2
@@ -37,6 +37,43 @@ for(int i = 0; i < data.length(); i++)
 {
 System.out.println(data);
 temp = "";
-}
-}
-}
+}}}
+*/
+
+//wap to accept a string in lowercase and change the first letter of every.
+import java.util.*;
+class Q3
+{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter words :");
+String str1 = sc.nextLine();
+String arr[] = str1.split(" ");
+for(int i = 0; i < arr.length; i++)
+{
+char ch = arr[i].charAt(0);
+String newstr = String.valueOf(ch).toUpperCase();
+String str2 = newstr + arr[i].substring(1);
+System.out.println(str2+ " ");
+}}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
