ebd449ac4d50c4d73adf9a0732b8c27b630563d3
Poojii001 <poojapal5781@gmail.com>
2025-10-26 22:02:27 +0530

    'Java-files'

--- String/sbfclass.java
+++ String/sbfclass.java
@@ -1,10 +1,23 @@
+/*
 class StringBufferExample
 {
 public static void main(String[] args)
 {
 StringBuffer sb = new StringBuffer("ducat");
 System.out.println(sb);
-sb.append("noida");
+sb.append(5,"noida");
+System.out.println(sb);
+}
+}
+*/
+//insert()
+class StringBufferExample
+{
+public static void main(String[] args)
+{
+StringBuffer sb = new StringBuffer("ducat");
+System.out.println(sb);
+sb.insert(5,"india");
 System.out.println(sb);
 }
 }
