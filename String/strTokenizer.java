1794eccb540082e3a4ab679fec733b670e5286ba
Poojii001 <poojapal5781@gmail.com>
2025-10-28 13:58:04 +0530

    'Java-files';

---
+++ String/strTokenizer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Tokenizer
+{
+public static void main(String[] args)
+{
+StringTokenizer str1 = new StringTokenizer("this is deepak");
+while(str1.hasMoreTokens())
+{
+System.out.println(str1.hasMoreTokens());
+}
+}}
