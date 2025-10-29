9950b7feb2a7332cb044e6762cc2533f8dd893fb
Poojii001 <poojapal5781@gmail.com>
2025-10-29 16:00:14 +0530

    'Java-files'

--- multiThreading.java
+++ multiThreading.java
@@ -18,7 +18,6 @@ System.out.println("Main thread");
 }
 }
 }
-*/
 
 class MyThread extends Thread
 {
@@ -33,6 +32,34 @@ public static void main(String[] ags)
 {
 MyThread t1 = new MyThread();
 t1.run();
+for(int i = 1; i<=5; i++)
+{
+System.out.println("Main thread");
+}
+}
+}
+*/
+//Overloading run():
+class MyThread extends Thread
+{
+public void run()
+{
+for(int i = 1; i<=5; i++)
+{
+System.out.println("User thread");
+}
+this.run(11);
+}
+public void run(int x)
+{
+for(int i = 1; i<=5; i++)
+{
+System.out.println("user2 thread");
+}}
+public static void main(String[] ags)
+{
+MyThread t1 = new MyThread();
+t1.start();
 for(int i = 1; i<=5; i++)
 {
 System.out.println("Main thread");
