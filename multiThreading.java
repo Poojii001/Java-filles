e4067f5864d7fe8966f2a4e450918a6ff6701d1a
Poojii001 <poojapal5781@gmail.com>
2025-10-31 08:13:38 +0530

    Java-files

--- multiThreading.java
+++ multiThreading.java
@@ -85,7 +85,7 @@ System.out.println(t2.getName());
 System.out.println(Thread.currentThread().getName());
 }
 }
-*/
+
 //setName():
 class MyThread extends Thread
 {
@@ -109,10 +109,28 @@ Thread.currentThread().setName("Pooja");
 System.out.println(Thread.currentThread().getName());
 }
 }
-
+*/
 
 //getId():
+class MyThread 
+{
+public void run()
+{
 
+}
+public static void main(String[] args)
+{
+MyThread t1 = new MyThread();
+t1.start();
+
+MyThread t2 = new MyThread();
+t2.start();
+
+System.out.println(t1.getId());
+System.out.println(t2.getId());
+System.out.println(Thread.currentThread().getId());
+}
+}
 //isAlive():
 
 
