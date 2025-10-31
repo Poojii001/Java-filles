211ea98384c9c2a4b8a206aef7f4c6e3fe62175c
Poojii001 <poojapal5781@gmail.com>
2025-10-31 08:56:59 +0530

    Th_c_Priority

--- Th_c_Priority.java
+++ Th_c_Priority.java
@@ -1,3 +1,4 @@
+/*
 class MyThread extends Thread
 {
 public void run()
@@ -14,4 +15,34 @@ System.out.println(Thread.NORM_PRIORITY);
 System.out.println(Thread.MAX_PRIORITY);
 //t1.setPriority();
 System.out.println(t1.getPriority());
+}}
+*/
+
+//sleep():
+
+class MyThread extends Thread
+{
+public void run()
+{
+for(int i = 1; i<=5; i++)
+{
+System.out.println("user thread");
+try 
+{
+Thread.sleep(1000);
+}
+catch(InterruptedException e)
+{
+System.out.println(e);
+}
+}}
+public static void main(String[] args)throws InterruptedException
+{
+MyThread t1 = new MyThread();
+t1.start();
+for(int i = 0; i <=5; i++)
+{
+System.out.println("main thread");
+Thread.sleep(1000);
+}
 }}
