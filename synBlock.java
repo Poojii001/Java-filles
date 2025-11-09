d85100ddcb79f18785a910826a91da2adb7b5543
Poojii001 <poojapal5781@gmail.com>
2025-11-09 20:00:52 +0530

    SYN-Methods

--- synBlock.java
+++ synBlock.java
@@ -47,8 +47,6 @@ demo2.start();
 Demo demo3 = new Demo(s,"Mahi");
 demo3.start();
 }}
-*/
-
 //Methods
 //await():
 
@@ -66,4 +64,28 @@ public static void main(String[] args)throws InterruptedException
 MyThread t1 = new MyThread();
 t1.start();
 System.out.println(t1.sum);
+}}
+*/
+class MyThread extends Thread
+{
+int sum = 0;
+public void run()
+{
+synchronized(this)
+{
+for(int i = 1; i <= 5; i++)
+{
+sum = sum+i;
+}
+this.notify();
+}}
+public static void main(String[] args)throws InterruptedException
+{
+MyThread t1 = new MyThread();
+t1.start();
+synchronized(t1)
+{
+t1.wait();
+System.out.println(t1.sum);
+}
 }}
