8d6dc61546dfb05ecdc53c6bc2623bf5536ad0ab
Poojii001 <poojapal5781@gmail.com>
2025-11-14 15:22:40 +0530

    FileHandling

--- FileHandling.java
+++ FileHandling.java
@@ -1,3 +1,5 @@
+/*
+File class 
 import java.io.*;
 class MyFile
 {
@@ -13,3 +15,22 @@ else
 {
 System.out.println("File is not present");
 }}}
+*/
+//FileCreate:
+
+import java.io.*;
+class MyFile
+{
+public static void main(String[] args)throws IOException
+{
+File file = new File("abc.txt");
+boolean b = file.createNewFile();
+if(b)
+{
+System.out.println("File is created");
+}
+else
+{
+System.out.println("File is not found");
+}}}
+
